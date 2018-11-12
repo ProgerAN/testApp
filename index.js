c599e361f7c0a698c827a40046e1df5c785a77f9
Artem ProgerAN <bomgik335533@gmail.com>
2018-11-12 21:53:38 +0400

    edit

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-bot.listen(process.env.PORT || 5000)
+require('https').createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
+  res.end('')
+});
 
 
 const TelegramBot = require('node-telegram-bot-api');
