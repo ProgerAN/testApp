4a800d4ef7c734379d2387d62d20758bcc7d17ef
Artem ProgerAN <bomgik335533@gmail.com>
2018-11-12 21:19:03 +0400

    edit

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const token = '678609158:AAEx7rtw_FXLDFwfSS1L6RDsGe-2Fu-nBoM';
 
 const bot = new TelegramBot(token, {polling: true});
 
-bot.listen(process.env.PORT || 3000 ,function(){
-  console.log("up and running on port "+process.env.PORT);
-});
+
 
 bot.onText(/\/echo (.+)/, (msg, match) => {
 
