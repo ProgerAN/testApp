fb56416b9177e3c459513bd55a891a6aeb1f5dae
Artem ProgerAN <bomgik335533@gmail.com>
2018-11-12 18:05:02 +0400

    edit

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-
+const TelegramBot = require('node-telegram-bot-api')
 
 const TOEKN = '678609158:AAEx7rtw_FXLDFwfSS1L6RDsGe-2Fu-nBoM'
+
+const bot = new TelegramBot(TOKEN, {polling: true})
+
+bot.on('message', msg => {
+  bot.sendMessage(msg.chat.id, `Hello drom HEROKU, bot sasy "${msg.from.first_name}"`)
+})
