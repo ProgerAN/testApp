cf5344157952587e0b47525e6d885c7a76a4fbc9
Artem ProgerAN <bomgik335533@gmail.com>
2018-11-12 22:16:16 +0400

    edit

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
-  bot.sendMessage(chatId, `Сообщения принял`);
+  bot.sendMessage(chatId, `Пошел нахуй ${msg.from.first_name}`);
 });
