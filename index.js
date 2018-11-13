8fc6c32329f58b8757a6a04806e4330d19aa445f
Artem ProgerAN <bomgik335533@gmail.com>
2018-11-13 17:41:53 +0400

    edit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const bot = new TelegramBot(token, {polling: true});
 var options = {
   reply_markup: JSON.stringify({
     inline_keyboard: [
-      [{ text: 'Кнопка 1', callback_data: '1' }],
+      [{ text: 'Кнопка 1', callback_data: 'test' }],
       [{ text: 'Кнопка 2', callback_data: 'data 2' }],
       [{ text: 'Кнопка 3', callback_data: 'text 3' }]
     ]
