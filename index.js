24d9aa7de2850deaab63e10db69ceece4f4053b7
Artem ProgerAN <bomgik335533@gmail.com>
2018-11-13 19:27:23 +0400

    efit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('http').createServer().listen(process.env.PORT || 5000).on('request', fu
 const TelegramBot = require('node-telegram-bot-api');
 
 // const token = ' ... ';
-const token = getenv('TOKEN')
+const token = getenv('TOKEN');
 
 const bot = new TelegramBot(token, {polling: true});
 
