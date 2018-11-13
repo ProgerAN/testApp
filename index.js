9c08737ebac72cf833f9ac869667c81585a25a28
Artem ProgerAN <bomgik335533@gmail.com>
2018-11-13 21:51:18 +0400

    edit

--- index.js
+++ index.js
@@ -10,20 +10,10 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.TOKEN; // Heroku configVar 
 const bot = new TelegramBot(token, {polling: true});
  
-bot.onText(/\/pask/, function (msg, match) {
-  bot.sendMessage(msg.chat.id, 'Ты нашел подсказку :)');
+
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, `Команда не распознана`);
 });
-
-// Написать мне ... (/echo Hello World! - пришлет сообщение с этим приветствием.)
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  const chatId = msg.chat.id; 
-  const resp = match[1];
-
-  bot.sendMessage(chatId, resp);
-});
-
-// bot.on('message', (msg) => {
-//   const chatId = msg.chat.id;
-
-//   bot.sendMessage(chatId, `Команда не распознана`);
-// });
