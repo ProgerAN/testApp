63ee5c41330bc88d6944bb4ae07f7df340193b26
Artem ProgerAN <bomgik335533@gmail.com>
2018-11-13 22:44:33 +0400

    edit

--- index.js
+++ index.js
@@ -30,14 +30,13 @@ bot.onText(/\/start/, (msg) => {
   });
 
 });
-
-var Pask = "Паскалка";
-if (msg.text.toString().toLowerCase().indexOf(Pask) === 0) {
-  bot.sendMessage(msg.chat.id, "Ты нашел пасхалку :)");
-}
-
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
-
-  bot.sendMessage(chatId, `Команда не распознана`);
+  var Pask = "Паскалка";
+  if (msg.text.toString().toLowerCase().indexOf(Pask) === 0) {
+    bot.sendMessage(msg.chat.id, "Ты нашел пасхалку :)");
+  }
+  else {
+    bot.sendMessage(chatId, `Команда не распознана`);
+  }
 });
