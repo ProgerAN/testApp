a61e3712d93f03d81a8a50afb64f9a8295b89390
Artem ProgerAN <bomgik335533@gmail.com>
2018-11-13 22:28:36 +0400

    edit

--- index.js
+++ index.js
@@ -23,9 +23,7 @@ bot.onText(/\/start/, (msg) => {
   bot.sendMessage(msg.chat.id, "Welcome", {
     "reply_markup": {
       "keyboard": [
-        ["Sample text", "Second sample"],
-        ["Keyboard"],
-        ["I'm robot"]
+        ["Паскалка", "Инфо"],
       ]
     }
   });
