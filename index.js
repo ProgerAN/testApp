7a219ca31236cf9b368e20ddbbb1992e55481043
Artem ProgerAN <bomgik335533@gmail.com>
2018-11-13 22:28:01 +0400

    edit

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
-require('http').createServer().listen(process.env.PORT || 5000).on('request', function(req, res){ res.end('') });
+require('http').createServer().listen(process.env.PORT || 5000).on('request', function (req, res) {
+  res.end('')
+});
 var reqTimer = setTimeout(function wakeUp() {
-  request("https://hero-bot-sp.herokuapp.com", function() {
-     console.log("WAKE UP DYNO");
+  request("https://hero-bot-sp.herokuapp.com", function () {
+    console.log("WAKE UP DYNO");
   });
   return reqTimer = setTimeout(wakeUp, 1200000);
 }, 1200000);
 
 const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.TOKEN; // Heroku configVar 
-const bot = new TelegramBot(token, {polling: true});
- 
+const bot = new TelegramBot(token, {
+  polling: true
+});
+
 
 
 
 
 bot.onText(/\/start/, (msg) => {
-  
+
   bot.sendMessage(msg.chat.id, "Welcome", {
     "reply_markup": {
-      "keyboard": [["Sample text", "Second sample"],   ["Keyboard"], ["I'm robot"]]
+      "keyboard": [
+        ["Sample text", "Second sample"],
+        ["Keyboard"],
+        ["I'm robot"]
+      ]
     }
   });
-  
+
 });
-    bot.on('message', (msg) => {
-      const chatId = msg.chat.id;
-      
-      bot.sendMessage(chatId, `Команда не распознана`);
-    });
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, `Команда не распознана`);
+});
